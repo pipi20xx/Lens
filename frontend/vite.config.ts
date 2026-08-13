94e9ef0a45b611ac4f1e3901d899df6e7fe79312
admin <admin@123.com>
2026-08-13 14:38:11 +0800

    3.0.5

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -69,7 +69,7 @@ export default defineConfig(({ mode }) => {
       isAnalyze && visualizer({ open: true, gzipSize: true, filename: 'dist/stats.html' }),
     ],
     define: {
-      __APP_VERSION__: JSON.stringify('3.0.4'),
+      __APP_VERSION__: JSON.stringify('3.0.5'),
     },
     resolve: {
       alias: {
